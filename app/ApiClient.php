46e096db0fd8b93f799ecfb6888c5090fc5a0487
Trotsijs <trotsijs@gmail.com>
2023-05-16 15:20:15 +0300

    added exceptions

--- app/ApiClient.php
+++ app/ApiClient.php
@@ -6,6 +6,7 @@ use App\Models\Article;
 use App\Models\Comment;
 use App\Models\User;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
 class ApiClient
 {
@@ -18,114 +19,151 @@ class ApiClient
 
     public function fetchArticles(): array
     {
-        $response = $this->client->get('https://jsonplaceholder.typicode.com/posts/');
-        $postData = json_decode($response->getBody()->getContents());
+        try {
+            $response = $this->client->get('https://jsonplaceholder.typicode.com/posts/');
+            $postData = json_decode($response->getBody()->getContents());
 
-        $postCollection = [];
+            $postCollection = [];
 
-        foreach ($postData as $post) {
-            $author = $this->fetchUserById($post->userId);
-            $postCollection[] = new Article($post->userId, $author->getUsername(), $post->id, $post->title,
-                $post->body);
+            foreach ($postData as $post) {
+                $author = $this->fetchUserById($post->userId);
+                $postCollection[] = new Article
+                (
+                    $post->userId,
+                    $author->getUsername(),
+                    $post->id,
+                    $post->title,
+                    $post->body
+                );
+            }
+
+            return $postCollection;
+
+        } catch (GuzzleException $exception) {
+            return [];
         }
-
-        return $postCollection;
     }
 
-    public function fetchSingleArticle(int $id): Article
+    public function fetchSingleArticle(int $id): ?Article
     {
 
-        $response = $this->client->get('https://jsonplaceholder.typicode.com/posts/' . $id);
-        $postData = json_decode($response->getBody()->getContents());
-        $author = $this->fetchUserById($postData->userId);
+        try {
+            $response = $this->client->get('https://jsonplaceholder.typicode.com/posts/' . $id);
+            $postData = json_decode($response->getBody()->getContents());
+            $author = $this->fetchUserById($postData->userId);
 
-        return new Article
-        (
-            $postData->userId,
-            $author->getUsername(),
-            $postData->id,
-            $postData->title,
-            $postData->body
-        );
+            return new Article
+            (
+                $postData->userId,
+                $author->getUsername(),
+                $postData->id,
+                $postData->title,
+                $postData->body
+            );
+
+        } catch (GuzzleException $exception) {
+            return null;
+        }
+
     }
 
     public function fetchUsers(): array
     {
-        $response = $this->client->get('https://jsonplaceholder.typicode.com/users');
-        $userData = json_decode($response->getBody()->getContents());
+        try {
+            $response = $this->client->get('https://jsonplaceholder.typicode.com/users');
+            $userData = json_decode($response->getBody()->getContents());
 
-        $userCollection = [];
+            $userCollection = [];
 
-        foreach ($userData as $user) {
-            $userCollection[] = new User
-            (
-                $user->id,
-                $user->username,
-                $user->name,
-                $user->email,
-                $user->address->city
-            );
+            foreach ($userData as $user) {
+                $userCollection[] = new User
+                (
+                    $user->id,
+                    $user->username,
+                    $user->name,
+                    $user->email,
+                    $user->address->city
+                );
+            }
+
+            return $userCollection;
+
+        } catch (GuzzleException $exception) {
+            return [];
         }
-
-        return $userCollection;
     }
 
-    public function fetchUserById($id): User
+    public function fetchUserById($id): ?User
     {
-        $response = $this->client->get('https://jsonplaceholder.typicode.com/users/' . $id);
-        $userData = json_decode($response->getBody()->getContents());
+        try {
+            $response = $this->client->get('https://jsonplaceholder.typicode.com/users/' . $id);
+            $userData = json_decode($response->getBody()->getContents());
 
-        return new User
-        (
-            $userData->id,
-            $userData->username,
-            $userData->name,
-            $userData->email,
-            $userData->website
-        );
+            return new User
+            (
+                $userData->id,
+                $userData->username,
+                $userData->name,
+                $userData->email,
+                $userData->website
+            );
+
+        } catch (GuzzleException $exception) {
+            return null;
+        }
     }
 
     public function fetchComments(int $id): array
     {
-        $response = $this->client->get('https://jsonplaceholder.typicode.com/comments?postId=' . $id);
-        $commentData = json_decode($response->getBody()->getContents());
+        try {
+            $response = $this->client->get('https://jsonplaceholder.typicode.com/comments?postId=' . $id);
+            $commentData = json_decode($response->getBody()->getContents());
 
-        $commentsCollection = [];
+            $commentsCollection = [];
 
-        foreach ($commentData as $comment) {
-            $commentsCollection[] = new Comment
-            (
-                $comment->postId,
-                $comment->id,
-                $comment->name,
-                $comment->email,
-                $comment->body
-            );
+            foreach ($commentData as $comment) {
+                $commentsCollection[] = new Comment
+                (
+                    $comment->postId,
+                    $comment->id,
+                    $comment->name,
+                    $comment->email,
+                    $comment->body
+                );
+            }
+
+            return $commentsCollection;
+
+        } catch (GuzzleException $exception) {
+            return [];
         }
-
-        return $commentsCollection;
     }
 
     public function fetchArticlesByUser(int $id): array
     {
-        $response = $this->client->get('https://jsonplaceholder.typicode.com/posts?userId=' . $id);
-        $postData = json_decode($response->getBody()->getContents());
+        try {
+            $response = $this->client->get('https://jsonplaceholder.typicode.com/posts?userId=' . $id);
+            $postData = json_decode($response->getBody()->getContents());
 
-        $postCollection = [];
+            $postCollection = [];
 
-        foreach ($postData as $post) {
-            $author = $this->fetchUserById($post->userId);
+            foreach ($postData as $post) {
+                $author = $this->fetchUserById($post->userId);
 
-            $postCollection[] = new Article
-            (
-                $post->userId,
-                $author->getUsername(),
-                $post->id,
-                $post->title,
-                $post->body
-            );
+                $postCollection[] = new Article
+                (
+                    $post->userId,
+                    $author->getUsername(),
+                    $post->id,
+                    $post->title,
+                    $post->body
+                );
+            }
+
+            return $postCollection;
+
+        } catch (GuzzleException $exception) {
+            return [];
         }
-
-        return $postCollection;
     }
 }
